03d4781a97d8558902d7311d658a45ab2522f5ba
singla70 <sanjaysingla39093@gmail.com>
2025-03-13 00:09:40 +0530

    Update day12.cpp

--- day12.cpp
+++ day12.cpp
@@ -19,3 +19,72 @@ public:
         
     }
 };
+
+
+// LEETCODE 34 FIRST AND LAST POSITION OF ELEMENT
+TC O(log n)
+SC O(1)
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+
+        int n= nums.size();
+        int l=0,h=n-1;
+        bool flag = false;
+        vector<int> v(2,-1);
+        int mid;
+
+
+        while(l<=h) {
+             mid = l+ (h-l)/2;
+
+            if(nums[mid] == target) {
+                if(mid==0 || nums[mid-1] != target) {
+                    v[0]=mid;
+                    break;
+                }
+                else h = mid-1;
+            }
+            else if(nums[mid]>target) h=mid-1;
+            else l= mid+1;
+        }
+        l=0,h=n-1;
+        while(l<=h) {
+          mid = l+ (h-l)/2;
+
+            if(nums[mid] == target) {
+                if(mid==n-1 || nums[mid+1] != target) {
+                    v[1]=mid;
+                    break;
+                }
+                else l=mid+1;
+            }
+            else if(nums[mid]>target) h=mid-1;
+            else l= mid+1;
+        }
+
+        return v;
+        
+    }  
+};
+
+//LEETCODE 35 SEARCH INSERT POSITION
+TC O(log n)
+SC O(1)
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n= nums.size();
+        int lo=0; int hi=n-1;
+        while(lo<=hi) {
+            int mid = lo + (hi-lo)/2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid]>target) hi = mid-1;
+            else lo=mid+1;
+        }
+        return lo;
+        
+    }
+};
